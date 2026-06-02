348f9696722a8529da9389da0e15e96df203898f
小楼一夜听风 <27588068@qq.com>
2026-06-02 16:37:57 +0800

    github actions 2 pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -51,7 +51,7 @@ routes.push({
 })
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
-  // base: '/ssp/',
+  base: process.env.VITE_BASE_PATH ?? '/',
   plugins: [vue()],
   resolve: {
     alias: {
